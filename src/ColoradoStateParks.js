9687f9eaed54c3ac6d86475fc9824b35be893e52
mutemip <paulmutemi2030@gmail.com>
2022-07-17 22:25:55 +0300

    task done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import {howManyParks} from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+
+  return (
+    
+    <div>
+      <h1>Colorado State Parks!</h1>
+      howManyParks();
+      <MesaVerde />
+      {trees}
+      wildlife();
+    </div>
+    
+  
+  )
 }
+export default ColoradoStateParks;
